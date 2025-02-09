4f8233e4babc03227a4ba7ef616de16baf67c2d1
Kevin Freitas Vieira da Silva <kevinfreitasv80@gmail.com>
2025-02-09 20:31:21 -0300

    commit #4

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,28 @@
 let speech = new SpeechSynthesisUtterance();
-
 let voices = [];
 
 const voiceSelect = document.querySelector("select");
 
-window.speechSynthesis.onvoiceschanged = () => {
+function loadVoices() {
   voices = window.speechSynthesis.getVoices();
+  if (voices.length === 0) {
+    setTimeout(loadVoices, 100);
+    return;
+  }
+
   speech.voice = voices[0];
 
-  if (voices) {
-    voices.forEach((voice, i) => {
-      let option = document.createElement("option");
-      option.textContent = voice.name;
-      option.value = i;
-      voiceSelect.appendChild(option);
-    });
-  } else {
-    alert("Not have voices in your browser");
-  }
-};
+  voices.forEach((voice, i) => {
+    let option = document.createElement("option");
+    option.textContent = voice.name;
+    option.value = i;
+    voiceSelect.appendChild(option);
+  });
+}
+
+loadVoices();
+
+window.speechSynthesis.onvoiceschanged = loadVoices;
 
 voiceSelect.addEventListener("change", () => {
   speech.voice = voices[voiceSelect.value];
